0b36cd7ef78313c90c4d20ba98fa1d249579be2a
DeeMcShmee <95409489+DeeMcShmee@users.noreply.github.com>
2023-06-21 13:28:30 -0400

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,11 +1,13 @@
 #Install PSWindowsUpdate
 Install-module PSWindowsUpdate -force
 
+# To install hyper-V if you're dealing with a VM
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
 # Good old windows update
 Get-WindowsUpdate -AcceptAll -Install -AutoReboot
 
-# To install hyper-V if you're dealing with a VM
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
 
 
 
