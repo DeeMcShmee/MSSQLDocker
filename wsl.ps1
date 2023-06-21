7ca0a71b234379e96c533216aadb6a36e0761187
DeeMcShmee <95409489+DeeMcShmee@users.noreply.github.com>
2023-06-20 20:27:06 -0400

    Create wsl.ps1

---
+++ wsl.ps1
@@ -0,0 +1,8 @@
+# To install wsl with latest ubuntu ....( I think )
+wsl --install -d Ubuntu
+
+# Need to set wsl to version 2
+wsl --set-default-version 2
+
+# The need to restart/shutdown wsl service
+wsl.exe --shutdown
