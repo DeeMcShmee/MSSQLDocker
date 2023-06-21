b8baaf8e03bf1aeea24eda615fb3f46e4d27527f
DeeMcShmee <95409489+DeeMcShmee@users.noreply.github.com>
2023-06-21 13:29:43 -0400

    Update Thelinuxside.sh

--- Thelinuxside.sh
+++ Thelinuxside.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
 #Install prerequisites
 sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg-agent -y
