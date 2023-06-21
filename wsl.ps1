8f65741147efd2c0de465d37f347da40b1d29ccc
DeeMcShmee <95409489+DeeMcShmee@users.noreply.github.com>
2023-06-21 14:55:16 -0400

    Update wsl.ps1

--- wsl.ps1
+++ wsl.ps1
@@ -1,11 +1,6 @@
 # To install wsl with latest ubuntu ....( I think )
 wsl --install -d Ubuntu
 
-# Need to set wsl to version 2
-wsl --set-default-version 2
-
-# The need to restart/shutdown wsl service
-#wsl.exe --shutdown
 
 # Need to reboot to Ubuntu to install completly
 restart-computer
